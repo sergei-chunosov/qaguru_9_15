6ae1b9d20e8158fdbf204d2ceb442e3f6508dc83
necros <necros83@gmail.com>
2024-01-19 09:42:16 +0530

    conftest.py add param

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,16 +1,34 @@
 import os
 import pytest
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
+# from selenium import webdriver
+# from selenium.webdriver.chrome.options import Options
 from selene import browser
 from utils import attach
+from dotenv import load_dotenv
+
+def pytest_addoption(parser):
+    parser.addoption(
+        '--browser_version',
+        default='100.0'
+    )
+
+
+DEFAULT_BROWSER_VERSION = '100.0'
 
 
 @pytest.fixture(scope='session', autouse=True)
-def setup_browser():
+def load_env():
+    load_dotenv()
+
+@pytest.fixture(scope='session', autouse=True)
+def setup_browser(request):
+    browser_version = request.config.getoption('--browser_version')
+    browser_version = browser_version if browser_version != "" else DEFAULT_BROWSER_VERSION
+
     options = Options()
     selenoid_capabilities = {
         "browserName": "chrome",
+        "browserVersion": browser_version,
         "selenoid:options": {
             "enableVNC": True,
             "enableVideo": True
